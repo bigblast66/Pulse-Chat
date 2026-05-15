010dadf01319d4950ddec4f1802719f06f69dad7
Akash Chebrolu <akashchebrolu66@gmail.com>
2026-05-15 21:57:19 +0530

    username auth and password req

--- backend/login/login.py
+++ backend/login/login.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import mysql.connector
+import aiomysql
 import bcrypt
 from email_validator import validate_email,EmailNotValidError
 from datetime import datetime,timezone
+from pymysql.err import IntegrityError
+import jwt
+import re
 
 
-def get_connection(db):
+async def get_connection(db):
     """
      connects to database and return the connection (MYSQL)
     """
-    return mysql.connector.connect(
+    return await aiomysql.connect(
         host="localhost",
         user="app_user",
         password="strong_password",
@@ -19,7 +22,7 @@ def get_connection(db):
     )
 
 
-def validate(email):
+def isValidEmail(email):
     """
     validate the input email and return boolean
     """
@@ -28,6 +31,49 @@ def validate(email):
         return True
     except EmailNotValidError:
         return False
+
+
+def isValidUserName(username):
+    USERNAME_REGEX = r"^[a-zA-Z0-9._]{3,30}$"
+    username = username.strip()
+    if not username:
+        return "Username is required"
+    
+    if not re.match(USERNAME_REGEX, username):
+        return (
+            "Username must be 3-30 characters and contain only "
+            "letters, numbers, dots, or underscores"
+        )
+
+    return None #valid
+
+def isValidPassword(password):
+    if len(password) < 8:
+        return "Password must be at least 8 characters"
+
+    if len(password) > 128:
+        return "Password too long"
+
+    if " " in password:
+        return "Password cannot contain spaces"
+
+    if not re.search(r"[A-Z]", password):
+        return "Password must contain an uppercase letter"
+
+    if not re.search(r"[a-z]", password):
+        return "Password must contain a lowercase letter"
+
+    if not re.search(r"\d", password):
+        return "Password must contain a number"
+
+    return None
+
+def generate_token(email):
+    payload={
+        "email":email,
+    }
+    pass
+
 
 app=FastAPI()
 app.add_middleware(
@@ -47,6 +93,7 @@ class user_input(BaseModel):
 #while signingup to make sure user didnt make typo in password
 class user_input_signup(BaseModel):
     email:str
+    username:str
     password:str
     confirm_password:str
 
@@ -54,76 +101,97 @@ class user_input_signup(BaseModel):
 #todo signup
 
 @app.post("/signup")
-def signup(x:user_input_signup):
-    password_check=x.password==x.confirm_password
-    email_check=validate(x.email)
-    if password_check and email_check:
-        pass
+async def signup(x:user_input_signup):
+    username=x.username.strip()
+    email=x.email.strip()
+    password_check=isValidPassword(x.password) is None and x.password==x.confirm_password
+    email_check=isValidEmail(email)
+    username_check=isValidUserName(username) is None
+
+    if password_check and email_check and username_check:
+        query="INSERT INTO user_metadata (email,password,creation_date,username)VALUES(%s,%s,%s,%s)"
+        creation_time=datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+
+
+        connection=await get_connection("chat_db")
+        cursor=await connection.cursor()
+
+
+        try:
+            
+            await cursor.execute(query,(email,bcrypt.hashpw(x.password.encode(), bcrypt.gensalt()).decode(),creation_time,username))
+            await connection.commit()
+            
+            
+            return{
+                "process":"signup",
+                "errors":0,
+                "status":"success"
+            }
+        except IntegrityError as e:
+            msg=str(e).lower()
+            if "email" in msg:
+                return{
+                    "process":"signup",
+                    "errors":1,
+                    "error1":"accountexisting"
+                }
+            if "username" in msg:
+                return{
+                    "process":"signup",
+                    "errors":1,
+                    "error1":"usernameexisting"
+                }
+            
+        finally:
+            await cursor.close()
+            connection.close()
     else:
-        if not password_check and not email_check:
-            return {
-                "process":"signup",
-                "errors":"2",
-                "error1":"notvalidemail",
-                "error2":"pwdnomatch"
-            }
-        elif not password_check:
-            return{
-                "process":"signup",
-                "errors":1,
-                "error1":"pwdnomatch"
-            }
-        else:
-            return{
-                "process":"signup",
-                "errors":1,
-                "error1":"notvalidemail"
-            }
-    query1="SELECT email FROM user_metadata WHERE email=%s"
-    query2="INSERT INTO user_metadata VALUES(%s,%s,%s)"
-    creation_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    connection=get_connection("chat_db")
-    cursor=connection.cursor()
-    cursor.execute(query1,(x.email,))
-    email_existing_acc=cursor.fetchone()
-    if email_existing_acc is not None:
-        return{
-            "process":"singup",
-            "errors":"1",
-            "error1":"accountexisting"
+        errors={
+            "process":"signup",
+            "errors":0
         }
-    cursor.execute(query2,(x.email,bcrypt.hashpw(x.password.encode(), bcrypt.gensalt()).decode(),creation_time))
-    return{
-        "process":"signup",
-        "error":"0",
-        "status":"success"
-    }
+        if not email_check:
+            errors["errors"]+=1
+            errors[f"error{errors['errors']}"]="notvalidemail"
+        if not password_check:
+            errors["errors"]+=1
+            errors[f"error{errors['errors']}"]="pwdnomatch"
+        if not username_check:
+            errors["errors"]+=1
+            errors[f"error{errors['errors']}"]="invalidusername"
+        return errors
 
 #otp verification also
 
 
 #todo tokenization for session management
 @app.post("/login")
-def login(x:user_input):
+async def login(x:user_input):
+    email=x.email.strip()
     query="SELECT password FROM user_metadata WHERE email=%s"
-    connection=get_connection("chat_db")
-    cursor=connection.cursor()
-    cursor.execute(query,(x.email,))
-    creds=cursor.fetchone()
-    cursor.close()
-    connection.close()
-    if creds is None:
+
+    connection=await get_connection("chat_db")
+    cursor=await connection.cursor()
+    try:
+        await cursor.execute(query,(email,))
+        creds=await cursor.fetchone()
+        
+        if creds is None:
+            return{
+                "process":"login",
+                "status":"fail"
+            }
+        if bcrypt.checkpw(x.password.encode(),creds[0].encode()):
+            return{
+                "process":"login",
+                "status":"success"
+            }
+        
         return{
             "process":"login",
-            "status":"createaccount"
+            "status":"fail"
         }
-    if bcrypt.checkpw(x.password.encode(),creds[0].encode()):
-        return{
-            "process":"login",
-            "status":"success"
-        }
-    
-    return{
-        "process":"login",
-        "status":"fail"
-    }
+    finally:
+        await cursor.close()
+        connection.close()
