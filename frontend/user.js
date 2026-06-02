449ecb1e2a13811b8d74e9f7857f1434ec2c1ecf
Akash Chebrolu <akashchebrolu66@gmail.com>
2026-06-02 21:57:38 +0530

    frontend changes/uv files

--- frontend/user.js
+++
@@ -1,7 +0,0 @@
-const person=document.getElementById("person")
-function addUser(username){
-    let x=document.createElement("div")
-    x.classList.add("user")
-    x.innerText=username
-    person.append(x)
-}
